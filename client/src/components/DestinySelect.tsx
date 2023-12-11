1496f22a7100641ca2ac28ea1e51b82b8a977fc2
Ignacio Keiniger <ignacio.keiniger@vestua.com>
2023-12-11 14:49:16 -0300

    feat: working destiny select in client

--- client/src/components/DestinySelect.tsx
+++ client/src/components/DestinySelect.tsx
@@ -1,26 +1,74 @@
 import { Select } from "antd";
-import { Option } from "antd/es/mentions";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-function DestinySelect() {
-    const [selectedDestiny, setSelectedDestiny] = useState(null);
+export type DestinyId = number
 
-    const destinies = [
-        { id: 1, name: 'Destino 1' },
-        { id: 2, name: 'Destino 2' },
-    ];
+export type DestinyData = {
+    id: DestinyId,
+    country_name: string,
+    external_id: number,
+    name: string,
+}
+
+const capitalizeFirstLetter = (s: string) => {
+    return s.charAt(0).toUpperCase() + s.slice(1).toLowerCase();
+}
+
+const filterOption = (input: string, option?: { label: string; value: string }) =>
+    (option?.label ?? '').toLowerCase().includes(input.toLowerCase());
+
+
+function DestinySelect({ setSelectedDestiny }: { setSelectedDestiny: (id: DestinyId) => void }) {
+    const [destinies, setDestinies] = useState<DestinyData[]>([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState<string | undefined>();
+
+    const onSearch = (value: string) => {
+        console.log('search:', value);
+    };
+
+
+    const selectOptions = () => destinies?.map((d) => ({ value: String(d.id), label: capitalizeFirstLetter(d.name) }))
+
+    const fetchDestinies = async () => {
+        setLoading(true);
+
+        try {
+            const res = await fetch(import.meta.env.VITE_SERVER_URL + "/destinies")
+
+            if (!res.ok) {
+                setError("Hubo un error al solicitar los destinos. Inténtalo de nuevo más tarde");
+                return;
+            }
+
+            const destinies = await res.json();
+            setDestinies(destinies);
+        } catch (error) {
+            setError("Hubo un error al solicitar los destinos. Inténtalo de nuevo más tarde");
+        }
+
+        setLoading(false);
+    }
+
+    useEffect(() => { fetchDestinies() }, [])
+
+    if (error) return <Select
+        status="error"
+        style={{ width: '11.5rem' }}
+        placeholder={error}
+    />
 
     return <Select
+        showSearch
         style={{ width: '11.5rem' }}
-        placeholder="Seleccionar destino"
-        onChange={(value) => setSelectedDestiny(value)}
-    >
-        {destinies.map((destiny) => (
-            <Option>
-                {destiny.name}
-            </Option>
-        ))}
-    </Select>
+        loading={loading}
+        disabled={loading}
+        placeholder={"Seleccionar destino"}
+        onChange={(value: DestinyId) => setSelectedDestiny(value)}
+        options={selectOptions()}
+        onSearch={onSearch}
+        filterOption={filterOption}
+    />
 }
 
 export default DestinySelect;
