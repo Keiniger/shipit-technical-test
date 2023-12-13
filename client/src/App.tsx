a15870956bd37c6c384f8ee0a94fed04a826ca11
Ignacio Keiniger <ignacio.keiniger@vestua.com>
2023-12-13 20:43:03 -0300

    feat: moved shipment components to ShipmentForm

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,53 +1,27 @@
-import { Typography, Row, Col, Layout, Menu, Space } from 'antd';
+import { Typography, Row, Col, Layout, Menu } from 'antd';
 import { Content, Header } from 'antd/es/layout/layout';
 const { Title } = Typography;
 
 import classes from './App.module.css';
 
-import DestinySelect, { DestinyId } from './components/DestinySelect';
-import DimensionsInput, { Dimension, Dimensions } from './components/DimensionsInputs';
-import Cotization from './components/Cotization';
-import CreateShipment from './components/CreateShipment';
-import ShipmentResult from './components/ShipmentResult';
-import { useState } from 'react';
+import ShipmentForm from './components/ShipmentForm';
 
 function App() {
-  const [selectedDestiny, setSelectedDestiny] = useState<DestinyId | undefined>();
-  const [dimensions, setDimensions] = useState<Dimensions>({
-    [Dimension.Length]: undefined,
-    [Dimension.Width]: undefined,
-    [Dimension.Height]: undefined,
-    [Dimension.Weight]: undefined,
-  });
-
-  return (
-    <Layout className={classes.layout}>
-      <Header className={classes.header}>
-        <Title
-          className={classes.title}
-          level={2}>
-          Prueba técnica de Shipit
-        </Title>
-        <Menu theme="dark" mode="horizontal" />
-      </Header>
-      <Content className={classes.content}>
-        <Row
-          className={classes.row}
-          justify="center"
-          align="middle">
-          <Col>
-            <Space direction="vertical" size="large">
-              <DestinySelect setSelectedDestiny={setSelectedDestiny} />
-              <DimensionsInput dimensions={dimensions} setDimensions={setDimensions} />
-              <Cotization selectedDestiny={selectedDestiny} dimensions={dimensions} />
-              <CreateShipment />
-              <ShipmentResult statusCode={undefined} />
-            </Space>
-          </Col>
-        </Row>
-      </Content>
-    </Layout>
-  );
+  return <Layout className={classes.layout}>
+    <Header className={classes.header}>
+      <Title className={classes.title} level={3}>
+        Prueba técnica de Shipit
+      </Title>
+      <Menu theme="dark" mode="horizontal" />
+    </Header>
+    <Content className={classes.content}>
+      <Row className={classes.row} justify="center" align="middle">
+        <Col>
+          <ShipmentForm />
+        </Col>
+      </Row>
+    </Content>
+  </Layout>
 };
 
 export default App
