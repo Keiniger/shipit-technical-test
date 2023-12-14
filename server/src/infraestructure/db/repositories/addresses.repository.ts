51de1e5300b15df5a7f6d95ece74bc2dd287a282
Ignacio Keiniger <ignacio.keiniger@vestua.com>
2023-12-14 15:06:12 -0300

    fix: address import typo

--- server/src/infraestructure/db/repositories/addresses.repository.ts
+++ server/src/infraestructure/db/repositories/addresses.repository.ts
@@ -1,7 +1,7 @@
-import Addres from '../models/address.model';
+import Address from '../models/address.model';
 
 export class AddressRepository {
   static async create(destiny_id: number, shipment_id: number) {
-    return Addres.create({ destiny_id, shipment_id });
+    return Address.create({ destiny_id, shipment_id });
   }
 }
