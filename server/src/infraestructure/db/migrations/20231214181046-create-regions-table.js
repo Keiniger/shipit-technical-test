64bbf22c0fbc80791977a2d3d90efdb1bf132fea
Ignacio Keiniger <ignacio.keiniger@vestua.com>
2023-12-14 16:25:39 -0300

    feat: added region model

---
+++ server/src/infraestructure/db/migrations/20231214181046-create-regions-table.js
@@ -0,0 +1,32 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('regions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('destinies');
+  },
+};
---
+++ server/src/infraestructure/db/models/region.model.ts
@@ -0,0 +1,33 @@
+import { DataTypes } from 'sequelize';
+import { sequelize } from '../database';
+
+const Region = sequelize.define(
+  'Region',
+  {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    createdAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+    updatedAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+  },
+  {
+    tableName: 'regions',
+  }
+);
+
+export default Region;
