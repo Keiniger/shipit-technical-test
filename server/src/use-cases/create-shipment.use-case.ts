8804871fc2c35858c382bc87c22c232d3eb49aad
Ignacio Keiniger <ignacio.keiniger@vestua.com>
2023-12-14 17:09:19 -0300

    feat: re-calculate cotization in the server

--- server/src/use-cases/create-shipment.use-case.ts
+++ server/src/use-cases/create-shipment.use-case.ts
@@ -1,14 +1,25 @@
 import { ShipmentEntity } from '../entities/shipment.entity';
 import { ShipmentRepository } from '../infraestructure/db/repositories/shipment.repository';
 import { AddressRepository } from '../infraestructure/db/repositories/addresses.repository';
+import getCotization from '../infraestructure/external/get-cotization';
 
 export default async function createShipmentUseCase(
   shipment: ShipmentEntity,
   destinyId: number
 ): Promise<ShipmentEntity> {
+  const cotization = await getCotization(destinyId, {
+    length: shipment.length,
+    width: shipment.width,
+    height: shipment.height,
+    weight: shipment.weight,
+  });
+
+  if (cotization?.courier !== shipment.courier || cotization.price !== shipment.price)
+    throw new Error('La cotización no es válida');
+
   const createdShipment = (await ShipmentRepository.create(shipment)) as unknown as ShipmentEntity;
 
-  if (!createdShipment.id) throw new Error('Error creating shipment');
+  if (!createdShipment.id) throw new Error('Error al crear el envio');
 
   await AddressRepository.create(destinyId, createdShipment.id);
 
